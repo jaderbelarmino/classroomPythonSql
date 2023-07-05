c2f41afc6a3533c46ef40525a26a63fe2d92af2f
jader belarmino <jader.belarmino@objectedge.com>
2023-07-05 20:21:37 -0300

    O(n)

---
+++ Assigment1_Q1.py
@@ -0,0 +1,21 @@
+from queue import LifoQueue
+
+# Initializing a stack
+
+def addToQueue(queue, element):
+    stack1 = []
+    for e in range(len(queue)):
+        stack1.append(queue.pop())
+    queue.append(element)
+    for e in range(len(stack1)):
+        queue.append(stack1.pop())
+
+    return queue
+
+myQueue = []
+myQueue = addToQueue(myQueue, "a")
+myQueue = addToQueue(myQueue, "b")
+myQueue = addToQueue(myQueue, "c")
+print(myQueue.pop())
+print(myQueue.pop())
+print(myQueue.pop())
